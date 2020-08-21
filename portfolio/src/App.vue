a77ec7302722cac3b4d6a9b910dc9d1a0e43d7b0
sully34 <sssulai@gmail.com>
2020-08-21 18:14:58 -0400

    added second card and second column, added second two cards and hyperlinks

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -23,32 +23,70 @@
             </b-card-text>
            </b-col>
          </b-list-group>
-          <b-col cols="10">
+        <b-col cols="10">
           <b-card title="" sub-title="">
           <b-card-text>
           <p class="text-left">   
           Ambitious, user-focused Full Stack Web Developer recently graduated from Columbia University Coding       Boot Camp with experience working     in teams. Possesses project management experience and experience      delegating responsibilities to a team from time at Maximus where he     spearheaded a project to create     the audio/ADA compliant portion of a new healthcare plan for the disabled distributed in the state          of New York. Sulai is flexible and a quick learner. Sulai is adaptable, communicative, knowledgeable     and steadfast with excellent     computer engineering skills learned in the Columbia University Coding     Bootcamp where he created projects in HTML5, CSS, JavaScript, and     React.</p> 
           </b-card-text>
-        <b-card-text></b-card-text>
-        <a href="#" class="card-link"></a>
-        <b-link href="#" class="card-link"></b-link>
+          <a href="#" class="card-link"></a>
+          <b-link href="#" class="card-link"></b-link>
           </b-card>
         </b-col>
-          <b-col cols="11">
+        
+        <b-col cols="8">
           <b-card title="" sub-title="">
           <b-card-text>
           <p class="text-left">   
-          APPLICATIONS BUILT
-          Project 1. Party Dash| https://partydash.com/
-          Front-End Design
-          •	We built Party Dash with a simple goal in mind: to help party hosts plan parties more easily
-          •	Responsibilities: Follow design specs handed down from site owner. Used CSS and HTML to create inviting website
-          Technologies used: HTML, CSS, Bootstrap
-          </p> 
+          <p>APPLICATIONS BUILT</p>
+          <hr>
+              <p class="text-left">Project 1. 
+              <p class="text-left">Front-End Design</p> 
+              <b-link href="https://partydash.com/">Party Dash|https://partydash.com/</b-link> 
+               
+              <p class="text-left">	We built Party Dash with a simple goal in mind: to help party hosts plan parties more easily
+              •	Responsibilities: Follow design specs handed down from site owner. Used CSS and HTML to create inviting website
+              Technologies used: HTML, CSS, Bootstrap</p>
+           
           </b-card-text>
-        <b-card-text></b-card-text>
-        <a href="#" class="card-link"></a>
-        <b-link href="#" class="card-link"></b-link>
+          <a href="#" class="card-link"></a>
+          <b-link href="#" class="card-link"></b-link>
+          </b-card>
+        </b-col>
+        <b-col cols="8">
+          <b-card title="" sub-title="">
+          <b-card-text>
+          <p class="text-left">   
+            <p class="text-left">Project 2.
+            <p class="text-left">Front-End design, Linting, Middleware, cleaning up frontend and backend code and making sure both sides work together.</p> 
+            <b-link href="https://runeskype.herokuapp.com/play">RuneSkype|https://runeskype.herokuapp.com/play</b-link> 
+            <p class="text-left">	RuneSkype is the newest way for friends to interact while playing your favorite Role Playing Game (RPG) RuneSkype. Playing RuneSkype is easy, just download a copy from our homepage or our repo.
+            <p class="text-left">Responsibilities:  Wrote clean, presentable code. Kept group on time and on track to present at Project Demo Day.
+            Technologies used: ReactJS, ChanceJS, Node, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios. Express, Faker, jQuery, React-dnd,  React-dnd-html5-backend,React-dom, React-draggable, React-router-dom, Webpack-dev-middleware, Webpack hot-middleware
+           </p>
+          </b-card-text>
+          <a href="#" class="card-link"></a>
+          <b-link href="#" class="card-link"></b-link>
+          </b-card>
+        </b-col>
+        <b-col cols="8">
+          <b-card title="" sub-title="">
+          <b-card-text>
+          <p class="text-left">   
+            <p class="text-left">Project 3.
+            <p class="text-left">Front-End Design, CSS, and front-end components.</p> 
+            <b-link href="http://www.goatvoter.org">RuneSkype|http://www.goatvoter.org/</b-link> 
+            <p class="text-left">	The GOAT Voter App is a one-stop platform where voters can access information about candidates running for political offices anywhere in the United States. The GOAT Voter App streamlines information and increases voter participation.
+            By entering their zip code in a search field, users will be able to search and access detailed information about all candidates running for election in their district. In addition, users can enter their address to find out where their nearest polling site is located and request            transportation on election day.
+            <p class="text-left">Responsibilities:  Wrote clean, presentable code. Kept group on time and on track to present at Project Demo Day.
+            Technologies used: ReactJS, ChanceJS, Node, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios. Express, Faker, jQuery, React-dnd,  React-dnd-html5-backend,React-dom, React-draggable, React-router-dom, Webpack-dev-middleware, Webpack hot-middleware
+           </p>
+           <p class="text-left">
+            Technologies used: JavaScript, Handlebars, Bootstrap, CSS Parallax, Node.js, Express, Sequelize, Google Civic Information API, Heroku
+           </p>
+          </b-card-text>
+          <a href="#" class="card-link"></a>
+          <b-link href="#" class="card-link"></b-link>
           </b-card>
         </b-col>
        </b-row>  
