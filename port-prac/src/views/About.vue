53de1b8347477e966c164ed3b5eb50d8b0b06076
sully34 <sssulai@gmail.com>
2020-08-26 16:27:34 -0400

    added card format and 3 routes

---
+++ port-prac/src/views/About.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-card title="Card title" sub-title="Card subtitle"></b-card>
+    <b-card-text>
+      Ambitious, user-focused Web and Software Developer with experience working
+      in teams and independently. Has strong project management experience and
+      experience delegating responsibilities to a team from time at Maximus
+      where he spearheaded a project to create the audio/ADA compliant portion
+      of a new healthcare plan for 1.5 million citizens of New York State. A
+      flexible and quick learner committed to constantly learning and evolving
+      his skills to stay abreast of ever-evolving technologies and trends in the
+      industry. Communicative, knowledgeable and steadfast with excellent
+      computer engineering skills learned in the Columbia University Coding
+      Bootcamp where he created projects in HTML5, CSS, JavaScript, VueJS, and
+      ReactJS.
+    </b-card-text>
+
+    <b-card-text>A second paragraph of text in the card.</b-card-text>
+
+    <a href="#" class="card-link">Card link</a>
+    <b-link href="#" class="card-link">Another link</b-link>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ port-prac/src/views/Experience.vue
@@ -0,0 +1,63 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div>
+    <div>
+      <b-card
+        title="Card Title"
+        img-src=""
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      />
+        <b-card bg-variant="success" text-variant="white" header="Success" class="text-center">
+          Project 1.
+          <p class="text-left"> 
+            Party Dash| https://partydash.com/ </p>
+        </b-card-text>
+        <b-card>
+          <p class="text-left" text-wrap>
+            Front-End Design • We Party Dash with a simple goal in mind: to help
+            party hosts plan parties more easily • Responsibilities: Follow
+            design specs handed down from site owner. Used CSS and HTML to
+            create inviting website
+          </p>
+          <p>class="text-left"Technologies used: HTML, CSS, Bootstrap</p>
+        </b-card>
+    </div>
+    <div>
+      <b-card
+        title="Card Title"
+        img-src=""
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      />
+        <b-card-text class="text-left">
+          RuneSkype
+          <p>
+           https://partydash.com/ </p>
+        </b-card-text class="text-left">
+        <b-card>
+          <p>
+            Project 2. RuneSkype| https://runeskype.herokuapp.com/play
+            Front-end design, Linting, cleaning up frontend and backend code and making sure both sides work together.
+            ●	RuneSkype is the newest way for friends to interact while playing your favorite Role Playing Game (RPG) RuneSkype. Playing RuneSkype is easy, just download a copy from our homepage or our repo:
+            ●	Responsibilities:  Wrote clean, presentable code. Kept group on time and on track to present at Project Demo Day.
+            Technologies used: ReactJS, ChanceJS, Node, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios. Express, Faker, jQuery, React-dnd, React-dnd-html5-backend,React-dom, React-draggable, React-router-dom, Webpack-dev-middleware, Webpack hot-middleware
+
+          </p>
+          <p class="text-left">Technologies used: HTML, CSS, Bootstrap</p>
+        </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
