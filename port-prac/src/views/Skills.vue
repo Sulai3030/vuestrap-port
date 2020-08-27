7d1f7252e8d28a976cd41082662c050609c4d541
sully34 <sssulai@gmail.com>
2020-08-27 17:51:40 -0400

    trying to add css to pages

--- port-prac/src/views/Skills.vue
+++ port-prac/src/views/Skills.vue
@@ -1,11 +1,65 @@
+/* eslint-disable prettier/prettier */
 <template>
-  <b-list-group>
-    <b-list-group-item>Cras justo odio</b-list-group-item>
-    <b-list-group-item active>Dapibus ac facilisis in</b-list-group-item>
-    <b-list-group-item>Morbi leo risus</b-list-group-item>
-    <b-list-group-item>Porta ac consectetur ac</b-list-group-item>
-    <b-list-group-item>Vestibulum at eros</b-list-group-item>
-</b-list-group>
+  <div>
+    <b-card
+      title="Card Title"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <b-card-text align="left" style="max-width: 20rem;">
+        <b-list-group>
+          <p align="left" style="max-width: 50rem;">
+            <b-list-group-item
+              >Languages: JavaScript, Python, CSS, HTML5</b-list-group-item
+            >
+          </p>
+          <b-list-group-item
+            >Libraries: VueJs, React.js, React Native, Express, Node JS, jQuery,
+            AJAX, Bootstrap</b-list-group-item
+          >
+          <p>
+            <b-list-group-item
+              >Databases: MongoDB, MySQL, NoSQL, Firebase</b-list-group-item
+            >
+          </p>
+          <p>
+            <b-list-group-item>Templates: Handlebars</b-list-group-item>
+          </p>
+          <p align="text-align:center">
+            <b-list-group-item>Version Control: Git/GitHub</b-list-group-item>
+          </p>
+          <p align="text-align:center">
+            <b-list-group-item>Testing: Jest/Enzyme</b-list-group-item>
+          </p>
+          <p align="text-align:center">
+            <b-list-group-item>Platforms: Drupal, WordPress</b-list-group-item>
+          </p>
+          <p align="text-align:center">
+            <b-list-group-item>Testing: Jest/Enzyme</b-list-group-item>
+          </p>
+          <p align="text-align:center">
+            <b-list-group-item
+              >Collaboration Software: Slack, Salesforce</b-list-group-item
+            >
+          </p>
+          <p align="text-align:center">
+            <b-list-group-item
+              >Image Editors: GIMP, Photoshop</b-list-group-item
+            >
+          </p>
+          <p align="text-align:center">
+            <b-list-group-item>Amazon Web Services (AWS)</b-list-group-item>
+          </p>
+          Creative Cloud: Photoshop, Illustrator, Lightroom
+        </b-list-group>
+      </b-card-text>
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+  </div>
 </template>
 
 <script>
